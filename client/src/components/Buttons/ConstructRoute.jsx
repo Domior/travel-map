d4c8ff6700a5fe66361bf68c5194143440168e35
Snisar Andrii <andrew.snisar@gmail.com>
2023-05-31 15:43:57 +0300

    added loader and fixed payment with animation

--- client/src/components/Buttons/ConstructRoute.jsx
+++ client/src/components/Buttons/ConstructRoute.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-import { MAP_MARKERS_LIMIT } from '../../constants/google_map';
-
-const ConstructRoute = ({ markersCount, onClick }) => {
+const ConstructRoute = ({ onClick, disabled }) => {
   return (
     <button
       type="button"
       className="btn-base fixed bottom-5 left-1/2 -translate-x-1/2"
-      disabled={markersCount !== MAP_MARKERS_LIMIT}
+      disabled={disabled}
       onClick={onClick}
     >
       Construct route
--- client/src/components/Dialogs/PaymentDetails.jsx
+++ client/src/components/Dialogs/PaymentDetails.jsx
@@ -1,60 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useStripe } from '@stripe/react-stripe-js';
+import { toast } from 'react-toastify';
+
+import Loader from '../Loader';
 
 import { MAP_MARKERS_LIMIT } from '../../constants/google_map';
 import { StripeService } from '../../services/StripeService';
 
-const PaymentDetails = ({ amount, markersCount }) => {
+const PaymentDetails = ({
+  amount,
+  markersCount,
+  isRouteConstructed,
+  onStartAnimation,
+}) => {
   const stripe = useStripe();
 
   const { profile } = useSelector(state => state.google);
   const { paymentMethod } = useSelector(state => state.stripe);
   const { markers } = useSelector(state => state.map);
 
+  const [isLoading, setIsLoading] = useState(false);
+
   const handlePayClick = async () => {
     if (!stripe) return;
 
     try {
+      setIsLoading(true);
       const { id: paymentMethodId } = paymentMethod;
-      const response = await StripeService.pay({
+      const {
+        data: { clientSecret },
+      } = await StripeService.pay({
         amount,
         paymentMethodId,
       });
 
-      console.log('[PaymentMethod]', paymentMethod);
-      console.log('[Response]', response);
+      const { error } = await stripe.confirmCardPayment(clientSecret);
 
-      const result = await stripe.confirmCardPayment(response.data.clientSecret);
-
-      if (result.error) {
-        alert('payment error');
+      if (error) {
+        toast.error('Something went wrong while processing the payment');
       } else {
-        alert('payment success');
-        console.log('result', result);
+        onStartAnimation();
+        toast.success('Payment was successful. Happy flight.');
       }
     } catch (err) {
-      console.log('[Error]', err);
+      toast.error(err.message);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
+    isRouteConstructed &&
     markersCount === MAP_MARKERS_LIMIT && (
       <div className="fixed max-w-md top-16 right-3 p-5 bg-white border rounded-lg ">
         <div className="mb-4">
+          <p className="mb-1 text-lg font-medium">Directions:</p>
           <p className="truncate">
             <span className="italic">From:</span> {markers[0]?.address}
           </p>
           <p className="truncate">
-            {' '}
-            <span className="italic">To:</span>{' '}
-            {markers[markers.length - 1]?.address}
+            <span className="italic">To:</span> {markers[markersCount - 1]?.address}
           </p>
         </div>
         <div className="mb-3">
-          <img className="mb-2 rounded-md" src={profile.picture} alt="user_image" />
-          <p>Name: {profile.name}</p>
-          <p>Email: {profile.email}</p>
+          <p className="mb-1 text-lg font-medium">Person information</p>
+          <div className="flex">
+            <img
+              className="mb-2 me-3 rounded-md"
+              src={profile.picture}
+              alt="user_image"
+            />
+            <div>
+              <p>Name: {profile.name}</p>
+              <p>Email: {profile.email}</p>
+            </div>
+          </div>
         </div>
         <div className="mb-5">
           <p>
@@ -66,7 +87,7 @@ const PaymentDetails = ({ amount, markersCount }) => {
           onClick={handlePayClick}
           disabled={!stripe}
         >
-          Pay
+          {!isLoading ? 'Pay' : <Loader />}
         </button>
       </div>
     )
